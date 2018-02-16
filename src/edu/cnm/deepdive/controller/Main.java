01cf32a42b016f0d0cecd239d8d8363e5412a633
DonnyStout <donnystout47@gmail.com>
2018-02-16 14:32:25 -0700

    Implemented game logic and logic to start new game.

--- src/edu/cnm/deepdive/controller/Main.java
+++ src/edu/cnm/deepdive/controller/Main.java
@@ -3,12 +3,14 @@ package edu.cnm.deepdive.controller;
 import edu.cnm.deepdive.util.Resources;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Collections;
 import java.util.List;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.text.Text;
 
@@ -38,6 +40,16 @@ public class Main implements EventHandler<ActionEvent> {
   private List<ToggleButton> tiles;
   private String totalStatusFormat;
   private String currentStatusFormat;
+  private ToggleButton button1;
+  private ToggleButton button2;
+  private String urlValue1;
+  private String urlValue2;
+  private int count = 1;
+  private int match = 0;
+  private int guess = 0;
+  private int gameSet = 0;
+  private int allGuess = 0;
+
 
   /**
    * Initialize the controller for the main view (layout). This includes loading
@@ -58,17 +70,81 @@ public class Main implements EventHandler<ActionEvent> {
     newGame(null);
   }
 
-  @Override
+
   public void handle(ActionEvent event) {
-    // TODO Add logic for handling clicks on tiles.
+    ToggleButton dis = ((ToggleButton) event.getSource());
+    if (count == 1) {
+      if (button1 != null) {
+        button1.setSelected(!button1.isSelected());
+        button1 = null;
+      }
+      if (button2 != null) {
+        button2.setSelected(!button2.isSelected());
+        button2 = null;
+      }
+      dis.setDisable(true);
+      urlValue1 = ((ImageView) dis.getGraphic()).getImage().getUrl();
+      button1 = dis;
+      count = 2;
+    } else {
+      urlValue2 = ((ImageView) dis.getGraphic()).getImage().getUrl();
+      button2 = dis;
+      guess++;
+      updateDisplay();
+      if (urlValue1.equals(urlValue2)) {
+        button2.setDisable(true);
+        match++;
+        if (match >= tiles.size() / 2) {
+          gameSet++;
+          allGuess += guess;
+          //  winSound();
+        }
+        updateDisplay();
+      } else {
+        button1.setDisable(false);
+      }
+//      two.setSelected(false);
+//      one.setSelected(false);
+      count = 1;
+    }
   }
 
   @FXML
   private void newGame(ActionEvent event) {
+    guess = 0;
+    match = 0;
+    count = 1;
+    button1 = null;
+    button2 = null;
     tileTable.getChildren().clear();
+    Collections.shuffle(tiles);
     for (ToggleButton tile : tiles) {
+      tile.setSelected(false);
+      tile.setDisable(false);
       tileTable.getChildren().add(tile);
+      updateDisplay();
     }
   }
+
+
+  private void updateDisplay() {
+    double avgGuess = (gameSet > 0) ? (double) allGuess / gameSet : 0;
+    currentStatus.setText(String.format(currentStatusFormat, guess, match));
+    totalStatus.setText(String.format(totalStatusFormat, gameSet, avgGuess));
+  }
+
+    public void quit() {
+      Platform.exit();
+      System.exit(0);
+    }
+
+  //  public void winSound() {
+//      // Open an audio input stream.
+//      File soundFile = new File("src/resources/win-effect.mp3");
+//      // Get a sound clip resource.
+//      win = new MediaPlayer(new Media(soundFile.toURI().toString()));
+//      // Open audio clip and load samples from the audio input stream.
+//      win.play();
+//  }
 
 }
